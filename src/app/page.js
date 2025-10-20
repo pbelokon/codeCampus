b7e9b645015eb7fe099c58d58c9ffb7488b9b661
pbelokon <pavelbelokon786@gmail.com>
2025-10-19 23:46:08 -0400

    another home page fix :)

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <article>
-      Welcome to CodeCampus, This is an open source learning platform! please
-      select a section to get started.
-    </article>
+    <div className="homepage">
+      <p>
+        Welcome to CodeCampus, This is an open source learning platform! please
+        select a section to get started.
+      </p>
+    </div>
   );
 }
